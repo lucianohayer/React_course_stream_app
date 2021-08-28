e6a4ca08b8c2c0618f2a967c9308f46e7ee6eaa3
Luciano Hayer <lucianohayer@gmail.com>
2021-08-28 22:28:34 +0200

    add userId to store

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -33,7 +33,7 @@ class GoogleAuth extends Component {
 
 	onAuthChange(isSignedIn) {
 		if (isSignedIn === true) {
-			this.props.signIn();
+			this.props.signIn(this.auth.currentUser.get().getId());
 		} else {
 			this.props.signOut();
 		}
