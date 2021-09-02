76ce54019eb1482b3febb4ef9a6775574d7031cf
Luciano Hayer <lucianohayer@gmail.com>
2021-09-02 22:57:47 +0200

    Implement show stream page

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-	return <div>StreamShow</div>;
+class StreamShow extends Component {
+	componentDidMount() {
+		const streamId = this.props.match.params.id;
+		this.props.fetchStream(streamId);
+	}
+
+	render() {
+		if (!this.props.stream) {
+			return <div>Loading...</div>;
+		}
+
+		const { title, description } = this.props.stream;
+
+		return (
+			<div>
+				<h1>{title}</h1>
+				<h5>{description}</h5>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	const streamId = ownProps.match.params.id;
+	return {
+		stream: state.streams[streamId],
+	};
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
